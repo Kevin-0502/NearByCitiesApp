7d15f1c4efec852b76bcfed6fc76bdb43464c644
Kevin Jiménez <69725320+Kevin-0502@users.noreply.github.com>
2026-06-02 18:46:45 -0600

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
-export const WEATHER_API_KEY = 'e7885c02c9da23125c7d66fd42af9482';
+export const WEATHER_API_KEY = '';
 export const WEATHER_BASE_URL = 'https://api.openweathermap.org/data/2.5';
 export const COUNTRIES_BASE_URL = 'https://restcountries.com/v3.1';
 
-export const GOOGLE_MAPS_API_KEY = 'AIzaSyDnPbjA2pjSpNfB8SfAkiYZt3Yf9FFK244';
+export const GOOGLE_MAPS_API_KEY = '';
